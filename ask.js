73872c7e5d20654bd78892c97fccc3d8ad8592d3
maikerulearncoding <maikerulearncoding@gmail.com>
2023-08-08 19:32:10 +0800

    ask.js added

--- ask.js
+++ ask.js
@@ -4,6 +4,35 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question("How do you like Node?", answer => {
-    console.log(`Your answer: ${answer}`);
-});
+const questions = [
+    "What is your name?",
+    "Where do you live?",
+    "What are you going to do with Node.js?"
+];
+
+function collectAnswers(questions, done) {
+    const answers = [];
+
+    const questionAnswered = (answer) => {
+        answers.push(answer.trim());
+        if(answers.length < questions.length) {
+            rl.question(
+                questions[answers.length],
+                questionAnswered
+            );
+        } else {
+            return done(answers);
+        }
+    };
+    rl.question(questions[0], questionAnswered);
+};
+
+collectAnswers(questions, (answers) => {
+    console.log("Thank you for your answers");
+    console.log(answers);
+    process.exit();
+})
+
+// rl.question("How do you like Node?", answer => {
+//     console.log(`Your answer: ${answer}`);
+// });
